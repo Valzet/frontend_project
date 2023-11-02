788255a9cd64f788b6ee58ae91bc463b2c28e706
Vlad <belousov7vlad@gmail.com>
2023-11-02 13:06:41 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const prices = [100, 150, 200, 250];
-const formattedPrices = prices.map((price) => price + ' р');
-console.log(formattedPrices);
+const prices = [100, 150, 200, 250]
+const formattedPrices = prices.map((price) => price + ' р')
+console.log(formattedPrices)
 
 const users = [
   ['alex', 32],
   ['tomas', 17],
   ['olga', 14],
   ['andre', 24],
-];
-const adults = users.filter((user) => user[1] > 18);
-console.log(adults);
+]
+const adults = users.filter((user) => user[1] > 18)
+console.log(adults)
 
 const products = [
   { title: 'пицца', price: 200 },
   { title: 'баранина', price: 300 },
   { title: 'креветки', price: 400 },
-];
-const totalCost = products.reduce((acc, product) => acc + product.price, 0);
-console.log(totalCost);
+]
+const totalCost = products.reduce((acc, product) => acc + product.price, 0)
+console.log(totalCost)
