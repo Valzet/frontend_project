550fe18677988f293694ae7796e117e1bc432871
Vlad <belousov7vlad@gmail.com>
2023-11-02 13:04:27 +0300

    chore: 3d lesson

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-const a = 'test'
+const prices = [100, 150, 200, 250];
+const formattedPrices = prices.map((price) => price + ' р');
+console.log(formattedPrices);
 
-console.warn(a)
-// const babel = 3 * a;
+const users = [
+  ['alex', 32],
+  ['tomas', 17],
+  ['olga', 14],
+  ['andre', 24],
+];
+const adults = users.filter((user) => user[1] > 18);
+console.log(adults);
 
-// function test() {
-//   return a + 3;
-// }
+const products = [
+  { title: 'пицца', price: 200 },
+  { title: 'баранина', price: 300 },
+  { title: 'креветки', price: 400 },
+];
+const totalCost = products.reduce((acc, product) => acc + product.price, 0);
+console.log(totalCost);
